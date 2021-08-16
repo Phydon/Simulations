3fe9972498fcffc132249c60d1b67b2c218a46c5
Phydon <84594612+Phydon@users.noreply.github.com>
2021-08-16 22:07:42 +0200

    Add files via upload

---
+++ ants/src/reproduction/simulsettings_reproduction.py
@@ -0,0 +1,90 @@
+import pygame as pg
+from random import randrange
+
+
+# setting environment variables -------------------------------------------
+settings = {}
+
+# paths
+settings['ASSETS_PATH'] = 'simulations\\ants\\assets'
+
+# (1000 is too big for 15,6'' screen)
+settings['HEIGHT'] = 720        # window height
+settings['WIDTH'] = 1000         # window width
+
+settings['SCREENRECT'] = pg.Rect(0, 0, settings['WIDTH'], settings['HEIGHT'])
+
+# window border
+settings['border_left'] = pg.Rect(
+    0, 0, settings['WIDTH'] - (settings['WIDTH'] - 5), settings['HEIGHT'])
+
+settings['border_right'] = pg.Rect(settings['WIDTH'] -
+                                   5, 0, settings['WIDTH'] -
+                                   (settings['WIDTH'] -
+                                    5), settings['HEIGHT'])
+
+settings['border_top'] = pg.Rect(
+    0, 0, settings['WIDTH'], settings['HEIGHT'] - (settings['HEIGHT'] - 5))
+
+settings['border_down'] = pg.Rect(0,
+                                  settings['HEIGHT'] - 5,
+                                  settings['WIDTH'],
+                                  settings['HEIGHT'] - (settings['HEIGHT'] - 5))
+
+settings['fps'] = 60           # frames per second
+
+# colours
+settings['WHITE'] = (255, 255, 255)
+settings['BLACK'] = (0, 0, 0)
+settings['BLUE'] = (0, 0, 255)
+settings['DARKVIOLETT'] = (25, 0, 51)
+settings['VIOLETT'] = (51, 0, 102)
+settings['RED'] = (255, 0, 0)
+settings['LIGHTRED'] = (255, 51, 51)
+settings['GREEN'] = (0, 255, 0)
+settings['DARKGREEN'] = (51, 102, 0)
+settings['PURPLE'] = (128, 0, 128)
+settings['YELLOW'] = (255, 255, 0)
+settings['ORANGE'] = (255, 165, 0)
+settings['GREY'] = (50, 50, 50)
+settings['TURQUOISE'] = (64, 224, 208)
+
+# setting number of entities
+settings['number_ants'] = 50
+settings['number_food'] = 50
+
+# setting properties
+settings['entity_width'] = 3
+settings['entity_height'] = 3
+settings['pheromone'] = []              # pheromone path
+settings['pheromone_tail'] = 600        # pheromone tail length
+settings['start_variance'] = 200        # variance of starting position
+
+# step size of entity, could lead to bug in collisions if not 1
+settings['velocity'] = 1
+
+# setting food properties
+settings['food_width'] = 8
+settings['food_height'] = 8
+settings['food_position_x'] = randrange(
+    1,
+    80,
+    2) if settings['velocity'] % 2 != 0 else randrange(
+        2,
+        80,
+    2)
+settings['food_position_y'] = randrange(
+    1,
+    80,
+    2) if settings['velocity'] % 2 != 0 else randrange(
+        2,
+        80,
+    2)
+
+# setting home properties
+settings['home_width'] = 15
+settings['home_height'] = 15
+settings['home_x'] = settings['WIDTH'] // 2 - \
+    settings['home_width'] // 2                     # home start x
+settings['home_y'] = settings['HEIGHT'] // 2 - \
+    settings['home_height'] // 2                    # home start y
